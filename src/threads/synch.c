92a3b0502a293b6f9bfb1802db87a5bf257b5d07
mirageoasis <kimhw0820@naver.com>
2025-05-16 10:46:28 +0900

    implemented semaphore priority

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -32,6 +32,16 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+/*
+user-defined function
+*/
+bool cmp_sem_priority(
+  const struct list_elem *a,
+  const struct list_elem *b,
+  void *aux UNUSED
+);
+
+
 /* Initializes semaphore SEMA to VALUE.  A semaphore is a
    nonnegative integer along with two atomic operators for
    manipulating it:
@@ -68,7 +78,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem, cmp_priority, NULL);
       thread_block ();
     }
   sema->value--;
@@ -113,10 +123,13 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+  if (!list_empty (&sema->waiters)){
+    list_sort(&(sema->waiters), cmp_priority, NULL);
+    thread_unblock (list_entry (list_pop_front (&sema->waiters),struct thread, elem));
+  }
   sema->value++;
+  // re schedule awaken thread compare to thread running now
+  test_max_priority();
   intr_set_level (old_level);
 }
 
@@ -335,4 +348,24 @@ cond_broadcast (struct condition *cond, struct lock *lock)
 
   while (!list_empty (&cond->waiters))
     cond_signal (cond, lock);
+}
+
+/*
+user-defined function
+*/
+
+/*
+compare elements in semaphore list
+return true if a is larger then b ellse 
+*/
+bool cmp_sem_priority(
+  const struct list_elem *a,
+  const struct list_elem *b,
+  void *aux UNUSED
+){
+  struct semaphore_elem *sa = list_entry(a,struct semaphore_elem, elem);
+  struct semaphore_elem *sb = list_entry(b,struct semaphore_elem, elem);
+
+  //return sa->priority > sb->priority;
+  return true;
 }
